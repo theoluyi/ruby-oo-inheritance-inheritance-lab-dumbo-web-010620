1804bde8e86fce3d7e305a5e2438b8758c2289c8
Theo Carney <theoluyi@gmail.com>
2020-12-21 18:50:43 -0500

    commit tree b650b34d40148ea149d17bfaeeb92afdb8b0fa48

---
+++ lib/user.rb
@@ -0,0 +1,11 @@
+class User
+  # create getter+setter methods for instances
+  attr_accessor :first_name, :last_name
+end
+
+# so this was breaking it because this is more than what they asked for
+# define the constructor/initialization process for a new instance
+# def initialize(first_name, last_name)
+#   @first_name = first_name
+#   @last_name = last_name
+# end
